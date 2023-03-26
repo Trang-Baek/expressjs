82d2f8687b84544e92913d96cc14760130f1611d
Trang <thutrang2k.work@gmail.com>
2023-03-26 16:48:02 +0700

    #router & controller

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ const app = express();
 const path = require("path");
 const port = 4000;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname,"public")));
 
+app.use(express.urlencoded({
+    extended:true
+}))//gửi từ form
+app.use(express.json())//từ js
 //HTTP logger
 app.use(morgan("combined"));
 
@@ -15,14 +21,26 @@ app.engine(".hbs", engine({extname:'.hbs'}));
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-    // res.send(`<h1 style="color:red;">Hello World!</h1>`)
-    res.render("home");
-});
-app.get("/news", (req, res) => {
-    // res.send(`<h1 style="color:red;">Hello World!</h1>`)
-    res.render("news");
-});
+route(app)
+
+// app.get("/", (req, res) => {
+//     // res.send(`<h1 style="color:red;">Hello World!</h1>`)
+//     res.render("home");
+// });
+// app.get("/news", (req, res) => {
+//     // res.send(`<h1 style="color:red;">Hello World!</h1>`)
+//     res.render("news");
+// });
+// app.get("/search", (req, res) => {
+//     // console.log(req.query)
+//     res.render("search");
+// });
+// 
+// //action --> dispatcher -> Function handler(controler)
+// app.post("/search", (req, res) => {
+//     console.log(req.body)//dùng middleware
+//     res.send("");
+// });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
