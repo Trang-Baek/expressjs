c5f1bdb0b6009d82c9471eeac3e4f51372c80905
Trang <thutrang2k.work@gmail.com>
2023-03-26 10:52:04 +0700

    push code

--- src/index.js
+++ src/index.js
@@ -5,15 +5,23 @@ const app = express();
 const path = require("path");
 const port = 4000;
 
+app.use(express.static(path.join(__dirname,"public")));
+
+//HTTP logger
 app.use(morgan("combined"));
 
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
+// template engine
+app.engine(".hbs", engine({extname:'.hbs'}));
+app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
 app.get("/", (req, res) => {
     // res.send(`<h1 style="color:red;">Hello World!</h1>`)
     res.render("home");
+});
+app.get("/news", (req, res) => {
+    // res.send(`<h1 style="color:red;">Hello World!</h1>`)
+    res.render("news");
 });
 
 app.listen(port, () => {
